cdd8ae5078d11dd509126749aa2daff64a45d54b
Sulliivan <qraks-@hotmail.fr>
2020-11-18 12:19:46 +0100

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mercrediAuthentication.Startup))]
+namespace mercrediAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
